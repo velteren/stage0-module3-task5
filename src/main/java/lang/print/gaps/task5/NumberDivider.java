d9d67b318398a9b67ecf54493941ed4d18876574
Constantine <77935386+velteren@users.noreply.github.com>
2023-12-17 19:17:13 +0300

    fix: errors

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        float result = toBeDivided / 5;
+        float result = (float)toBeDivided / 5;
         System.out.println(result);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
-        int seconds = (int) (minutes * 60);
+        float seconds = (int) (minutes * 60);
         System.out.println(seconds);
     }
 }
