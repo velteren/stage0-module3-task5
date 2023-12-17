63d6b69b119e92f1d607e4465f612f91a531bbb7
Constantine <77935386+velteren@users.noreply.github.com>
2023-12-17 19:20:51 +0300

    fix: errors

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
-        float seconds = (int) (minutes * 60);
+        float seconds = minutes * 60;
         System.out.println(seconds);
     }
 }
